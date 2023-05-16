df66bf59e65cd3b32684930ca3e28ec8dad70eb5
desmond <desmondnelson908@gmail.com>
2023-05-16 22:50:19 +0000

    register

---
+++ lib/model/user_model.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class User {
+  final String id;
+  final String name;
+  final bool isOnline;
+  User({
+    required this.id,
+    required this.name,
+    required this.isOnline,
+  });
+
+  User copyWith({
+    String? id,
+    String? name,
+    bool? isOnline,
+  }) {
+    return User(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      isOnline: isOnline ?? this.isOnline,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'isOnline': isOnline,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['id'] as String,
+      name: map['name'] as String,
+      isOnline: map['isOnline'] as bool,
+    );
+  }
+
+  @override
+  String toString() => 'User(id: $id, name: $name, isOnline: $isOnline)';
+
+  @override
+  bool operator ==(covariant User other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.id == id &&
+      other.name == name &&
+      other.isOnline == isOnline;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ name.hashCode ^ isOnline.hashCode;
+}
--- lib/remote_data_source/remote_data_source.dart
+++ lib/remote_data_source/remote_data_source.dart
@@ -1,7 +1,24 @@
+import 'package:chat_app/core/failure.dart';
+import 'package:chat_app/core/typedef.dart';
+import 'package:chat_app/model/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RemoteDataSource{
  final  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+ late SharedPreferences preferences;
  
+ FutureEither<void> register({required User user})async{
+  preferences = await SharedPreferences.getInstance();
+  try {
+    await _firestore.collection('users').doc(user.id).set(user.toMap());
+    preferences.setString('id', user.id);
+    return right(null);
+  } catch (e) {
+    return left(Failure(e.toString()));
+  }
+ }
  
+
 }
